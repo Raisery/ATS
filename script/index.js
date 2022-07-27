1d0e539a4a77d2effdabc820141edb4cfbfaf241
Lucas <lucas1.gerard@gmail.com>
2022-07-27 20:24:26 +0200

    taxis page creation

--- script/index.js
+++ script/index.js
@@ -1,9 +1,12 @@
-function scrollToTarget(idTarget) {
-    const target = document.getElementById(idTarget);
-    if(!target) return
-    window.scrollTo({
-        top: target.offsetTop-150,
-        left: target.offsetLeft,
-        behavior: 'smooth'
-      });
+const ROUTES = [
+    {
+        path: "taxis.html",
+        name: "taxis"
+    }
+]
+
+function router(target) {
+    const route = ROUTES.find(r => r.name == target )
+    if(!route) return document.body.innerHTML = `<h1>ERREUR 404</h1>`;
+    window.location.href = route.path;
 }
---
+++ script/taxis.js
@@ -0,0 +1,42 @@
+async function getProfilsData() {
+    const profils = await fetch("../data/profils.json")
+        .then(response => {
+            return response.json();
+        })
+    return profils
+}
+
+function getProfilUI(profil) {
+    const card = document.createElement("div");
+    card.classList.add('card-profil');
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('card-profil__img');
+    const photo = document.createElement("img");
+    photo.setAttribute("src",`./images/profils/${profil.id}/ensemble.jpg`);
+    const header = document.createElement("div");
+    header.classList.add('card-profil__header');
+    const name = document.createElement("p");
+    name.classList.add('card-profil__header__name');
+    name.textContent = `${profil.prenom} ${profil.nom}`;
+    const num = document.createElement("p");
+    num.classList.add('card-profil__header__num');
+    num.textContent = profil.id
+
+    imgContainer.appendChild(photo);
+    card.appendChild(imgContainer);
+    header.appendChild(name);
+    header.appendChild(num);
+    card.appendChild(header);
+
+    return card
+}
+
+async function displayProfils() {
+    const section = document.querySelector(".profils-container");
+    const profils = await getProfilsData();
+    for(let profil of profils) {
+        section.appendChild(getProfilUI(profil))
+    }
+}
+
+displayProfils()
